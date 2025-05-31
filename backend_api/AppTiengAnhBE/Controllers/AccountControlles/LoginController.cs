d34bfc2dba0a1d03953af3ad41234bf8efbaaa7c
Minh Tho <nguyenminhtho0503@gmail.com>
2025-05-31 23:48:08 +0700

    tao folder

---
+++ backend_api/AppTiengAnhBE/Controllers/AccountControlles/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppTiengAnhBE.Controllers.AccountControlles
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
